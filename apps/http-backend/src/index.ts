1fcf67c0144f6732c6952e7a4655357147c7e245
HimanshuGolyan01 <golyanhimanshu@gmail.com>
2025-12-08 23:50:09 +0530

    ws-backend logic

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express"
+import { Jwt } from "jsonwebtoken";
+import { config } from "dotenv";
+import { middleware } from "./middleware";
+const app = express();
+
+app.post("signup", (req, res) => {
+
+    res.json({
+        userId : "123"
+    })
+
+})
+
+app.post("signin", (req, res) => {
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET)
+    res.json({
+        token
+    })
+})
+
+app.post("room",middleware, (req, res) => {
+
+    res.json({
+        roomId : 123
+    })
+})
+
+app.listen(3000);
