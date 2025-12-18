0cc7290c55ac653cfb4f58cbebd097c0677fc097
HimanshuGolyan01 <golyanhimanshu@gmail.com>
2025-12-18 17:45:04 +0530

    httpbackend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,37 +1,141 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from "./middleware";
-import { JWT_SECRET } from "@repo/backend-common/config";
 import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
+import { prisma } from "@repo/db"; 
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
-app.post("/signup", (req, res) => {
-    const data = CreateUserSchema.safeParse(req.body);
+app.post("/signup", async (req, res) => {
 
-    if (!data.success) {
-        return res.status(400).json({ message: "incorrect input" });
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        console.log(parsedData.error);
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+    try {
+        const user = await prisma.user.create({  
+            data: {
+                email: parsedData.data.username,
+                password: parsedData.data.password,
+                name: parsedData.data.name
+            }
+        })
+        res.json({
+            userId: user.id
+        })
+    } catch(e) {
+        res.status(411).json({
+            message: "User already exists with this username"
+        })
+    }
+})
+
+app.post("/signin", async (req, res) => {
+    const parsedData = SigninSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
     }
 
-    return res.json({
-        message: "signup successful",
-        data: data.data
+    const user = await prisma.user.findFirst({ 
+        where: {
+            email: parsedData.data.username,
+            password: parsedData.data.password
+        }
+    })
+
+    if (!user) {
+        res.status(403).json({
+            message: "Not authorized"
+        })
+        return;
+    }
+
+    const token = jwt.sign({
+        userId: user?.id
+    }, JWT_SECRET);
+
+    res.json({
+        token
+    })
+})
+
+app.post("/room", middleware, async (req, res) => {
+    const parsedData = CreateRoomSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+    
+    const userId = req.userId;
+
+    try {
+        const room = await prisma.room.create({  
+            data: {
+                slug: parsedData.data.name,
+                adminId: userId
+            }
+        })
+
+        res.json({
+            roomId: room.id
+        })
+    } catch(e) {
+        res.status(411).json({
+            message: "Room already exists with this name"
+        })
+    }
+})
+
+app.get("/chats/:roomId", async (req, res) => {
+    try {
+        const roomId = Number(req.params.roomId);
+        console.log(req.params.roomId);
+        const messages = await prisma.chat.findMany({  // Changed this
+            where: {
+                roomId: roomId
+            },
+            orderBy: {
+                id: "desc"
+            },
+            take: 1000
+        });
+
+        res.json({
+            messages
+        })
+    } catch(e) {
+        console.log(e);
+        res.json({
+            messages: []
+        })
+    }
+    
+})
+
+app.get("/room/:slug", async (req, res) => {
+    const slug = req.params.slug;
+    const room = await prisma.room.findFirst({  // Changed this
+        where: {
+            slug
+        }
     });
-});
 
-app.post("/signin", (req, res) => {
-    const userId = 1;
+    res.json({
+        room
+    })
+})
 
-    const token = jwt.sign({ userId }, JWT_SECRET);
-
-    return res.json({ token });
-});
-
-app.post("/room", middleware, (req, res) => {
-    return res.json({
-        roomId: 123
-    });
-});
-
-app.listen(3000, () => console.log("server vibing on 3000"));
+app.listen(3001);
